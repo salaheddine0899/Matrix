0e7a4388ea8d0b5f4f86aa716992ff23b15cc1d9
Salah Eddine <salaheddineabid99@gmail.com>
2021-10-29 20:19:19 +0100

    first commit

---
+++ MatrixClient/MatrixClient.cpp
@@ -0,0 +1,30 @@
+// MatrixClient.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "Matrice.h"
+using namespace LA;
+int main()
+{
+    Matrice m(2,2);
+    m.Remplissage(2);
+    m.print();
+    Matrice m2(2, 2);
+    m2.Remplissage(1);
+    Matrice m3 = m + m2;
+    m3.print();
+    m3 = m * 3;
+    m3.print();
+    std::cout << "Hello World!\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
